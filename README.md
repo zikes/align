5d167bd5349fcf56432318718117366e01242b4e
Jason Hutchinson <jhutchinson@wehco.com>
2019-01-18 11:47:12 -0600

    basic documentation

---
+++ README.md
@@ -0,0 +1,22 @@
+# Align
+
+## Center
+
+```go
+fmt.Println("<"+align.Center(20, "something")+">")
+// <     something      >
+```
+
+## Right
+
+```go
+fmt.Println("<"+align.Right(20, "something")+">")
+// <           something>
+```
+
+## Left
+
+```go
+fmt.Println("<"+align.Left(20, "something")+">")
+// <something           >
+```
--- align.go
+++ align.go
@@ -2,14 +2,17 @@ package align
 
 import "fmt"
 
+// Center will center text within a given length of space
 func Center(w int, s string) string {
 	return fmt.Sprintf("%[1]*s", -w, fmt.Sprintf("%[1]*s", (w+len(s))/2, s))
 }
 
+// Right will right-align text within a given length of space
 func Right(w int, s string) string {
 	return fmt.Sprintf("%[1]*s", w, s)
 }
 
+// Left will left-align text within a given length of space
 func Left(w int, s string) string {
 	return fmt.Sprintf("%[1]*s", -w, s)
 }
